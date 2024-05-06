e5c5e86aec57300a6fe66a797c6c979bdf7d19b3
Luciano Rivera <lucianonicolasrivera8@gmail.com>
2024-05-06 19:28:39 -0300

    integrando api del clima

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-const apiKey = "";
-const apiUrl = "";
+const apiKey = "a82d98030416d40eba851bfc7b856a75";
+const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?units=metric&=London";
+
+async function checkWeater() {
+  const response = await fetch(apiUrl + `&appid=${apiKey}`);
+  let data = await response.json();
+
+  console.log(data);
+
+
+}
+checkWeater();
